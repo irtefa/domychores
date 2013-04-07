3a2d7f399d6ca5b988be25dabf0d1b539059a38c
Mohd Irtefa <irtefa1@illinois.edu>
2013-04-06 19:42:57 -0500

    added user signup mockup

--- handlers/signup.py
+++ handlers/signup.py
@@ -1,7 +1,21 @@
 from tornado.web import RequestHandler
+import simplejson as json
 
 
 class SignupHandler(RequestHandler):
 
     def post(self):
         #self.render("about.html")
+        body = json.loads(self.request.body)
+        user = body["user"]
+        if self.add_user(user):
+            self.write({"success": "true"})
+        else:
+            self.write({"success": "false"})
+        self.finish()
+
+    def add_user(self, user):
+        # check if the user alredy exists
+        # return false if user already exists
+        # else add to the database
+        return True
