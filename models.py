ccf19d28e751b7a5cfd6ec21b5ab3755e4c22a98
Rishi Bajekal <rishi.bajekal@gmail.com>
2013-04-11 22:03:56 -0500

    Added testing for chore handlers. Asynchronous calls still seem to mess up sequential tests

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.schema import ForeignKey
+from sqlalchemy.engine import reflection
+from sqlalchemy.schema import ForeignKey, MetaData, Table, DropTable, ForeignKeyConstraint, DropConstraint
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine("mysql://root:@localhost/domychores")
@@ -58,6 +59,35 @@ class Chore(Base):
 def create_all():
     """Create all the Tables"""
     Base.metadata.create_all(engine)
+
+
+def drop_all_tables():
+    """Drops all the Tables"""
+    # Taken from http://www.sqlalchemy.org/trac/wiki/UsageRecipes/DropEverything
+    conn = engine.connect()
+    trans = conn.begin()
+    inspector = reflection.Inspector.from_engine(engine)
+    metadata = MetaData()
+
+    tbs = []
+    all_fks = []
+    for table_name in inspector.get_table_names():
+        fks = []
+        for fk in inspector.get_foreign_keys(table_name):
+            if not fk['name']:
+                continue
+            fks.append(ForeignKeyConstraint((), (), name=fk['name']))
+        t = Table(table_name, metadata, *fks)
+        tbs.append(t)
+        all_fks.extend(fks)
+
+    for fkc in all_fks:
+        conn.execute(DropConstraint(fkc))
+
+    for table in tbs:
+        conn.execute(DropTable(table))
+
+    trans.commit()
 
 
 if __name__ == '__main__':
--- test/chore_test.py
+++ test/chore_test.py
@@ -4,7 +4,7 @@ os.sys.path.insert(0, parentdir)
 import unittest
 import json
 from server import Application
-from models import User, Chore
+from models import Chore, drop_all_tables, create_all
 from tornado.testing import AsyncHTTPTestCase
 from functools import partial
 
@@ -12,9 +12,16 @@ from functools import partial
 class ChoreTest(AsyncHTTPTestCase):
     """A TestCase containing various tests for the CreateChore, AcceptChore, WithdrawChore, and RemoveChore request handlers."""
 
+    @staticmethod
+    def clean_up():
+        """Drop tables and create again to remove all test data."""
+        drop_all_tables()
+        create_all()
+
     def setUp(self):
         """Set up to be done before each test."""
         super(ChoreTest, self).setUp()
+        self.db = self.get_app().db
         self.user_api = partial(self.fetch, "/api/signup", method="POST")
 
         post_data = {"first_name": "John",
@@ -46,33 +53,91 @@ class ChoreTest(AsyncHTTPTestCase):
         return Application()
 
     def test_chore_cycle(self):
-        """Tests creation, update, windrawal, and removal of a chore."""
-        chore = {"task": "Do my laundry",
-                 "description": "Figure it out. Its not that hard",
-                 "owner_id": 1}
-        response = self.create_api(body=json.dumps(chore)).body
-        # Test that response is success
-        expected = {"success": True}
-        self.assertEqual(response, json.dumps(expected))
+        """Tests creation, update, withdrawal, and removal of a chore."""
+        self.__chore_creation(self.db)
+        # self.__chore_update(self.db)
+        # self.__chore_withdraw(self.db)
+        # self.__chore_delete(self.db)
 
     def tearDown(self):
-        """Tear down users that were created."""
-        db = self.get_app().db
-        john = db.query(User).filter(User.email == "john.doe@fail.com").one()
-        jane = db.query(User).filter(User.email == "jsmith@fail.com").one()
-        bob = db.query(User).filter(User.email == "blah@fail.com").one()
-        chore = db.query(Chore).filter(Chore.task == "Do my laundry").one()
-        # Remove inserted users and chore from database
-        db.delete(chore)
-        db.commit()
-        db.delete(john)
-        db.delete(jane)
-        db.delete(bob)
-        db.commit()
+        """Cleans up after the test."""
+        drop_all_tables()
+        create_all()
 
     def runTest(self):
         """Runs all unit tests for this class"""
         unittest.main()
+        ChoreTest.clean_up()
+
+    def __chore_creation(self, db):
+        """Create chore and and test API response and database."""
+        chore = {"task": "Do my laundry",
+                 "description": "Figure it out. Its not that hard",
+                 "owner_id": 1}
+
+        response = None
+        while response is None:
+            response = self.create_api(body=json.dumps(chore)).body
+
+        # Test that response is success
+        expected = {"success": True}
+        self.assertEqual(response, json.dumps(expected))
+
+        # Test that database contains new chore
+        # chore_in_db = db.query(Chore).filter(Chore.owner_id == chore["owner_id"]).one()
+        # self.assertIsNotNone(chore_in_db)
+
+    def __chore_update(self, db):
+        """Update chore with worker and test API response and database."""
+        chore_update = {"id": 1,
+                        "worker_id": 2}
+        response = None
+        while response is None:
+            response = self.accept_api(body=json.dumps(chore_update)).body
+
+        # Test that response is success
+        expected = {"success": True}
+        self.assertEqual(response, json.dumps(expected))
+
+        # Test that database contains updated chore info
+        chore_in_db = db.query(Chore).filter(Chore.worker_id == chore_update["worker_id"]).one()
+        self.assertIsNotNone(chore_in_db)
+
+    def __chore_withdraw(self, db):
+        """Withdraw worker from chore and test API response and database."""
+        chore_update = {"id": 1,
+                        "worker_id": 2}
+
+        response = None
+        while response is None:
+            response = self.withdraw_api(body=json.dumps(chore_update)).body
+
+        # Test that response is success
+        expected = {"success": True}
+        self.assertEqual(response, json.dumps(expected))
+
+        # Test that database contains updated chore info
+        # chore_in_db = db.query(Chore).filter(Chore.worker_id == chore_update["worker_id"]).one()
+        # self.assertIsNone(chore_in_db)
+
+    def __chore_delete(self, db):
+        """Delete chore and test API response and database."""
+        delete_chore = {"id": 1,
+                        "worker_id": 2}
+
+        response = None
+        while response is None:
+            response = self.remove_api(body=json.dumps(delete_chore)).body
+
+        # Test that response is success
+        expected = {"success": True}
+        self.assertEqual(response, json.dumps(expected))
+
+        # Test that database contains updated chore info
+        # chore_in_db = db.query(Chore).filter(Chore.id == delete_chore["id"]).one()
+        # self.assertIsNone(chore_in_db)
+
 
 if __name__ == '__main__':
     unittest.main()
+    ChoreTest.clean_up()
