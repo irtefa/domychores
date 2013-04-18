0daa71d447f680b73ef8b45fc87b5cd7052f675b
Mohd Irtefa <irtefa1@illinois.edu>
2013-04-18 00:04:40 -0500

    not able to browse on the frontend yet

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,37 @@
-App = Ember.Application.create();
+window.App = Ember.Application.create({
+    LOG_TRANSITIONS: true
+});
+
+DS.RESTAdapter.reopen({
+  namespace: 'api'
+});
+
+App.Store = DS.Store.extend({
+    revision: 12
+});
+
+App.Chore = DS.Model.extend({
+    id: DS.attr('number'),
+    task: DS.attr('string'),
+    description: DS.attr('string'),
+    ownerId: DS.attr('number'),
+    workerId: DS.attr('number'),
+    postedAt: DS.attr('date')
+});
+
+App.Router.map(function() {
+    this.resource('chores', {path: 'chores'});
+    this.route('about');
+});
+
+App.ChoresRoute = Ember.Route.extend({
+    setupController: function(controller, model) {
+        controller.set('content', model);
+    },
+    model: function() {
+        return App.Chore.find();
+    }
+});
+
+
 
